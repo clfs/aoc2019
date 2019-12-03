876c66f33bac184419201c1da7fbf819090d33f8
Calvin Figuereo-Supraner <mail@calvin.page>
2019-12-03 12:38:09 -0800

    Move functions around

--- test_day03.py
+++ test_day03.py
@@ -19,6 +19,17 @@ def to_moves(wire: str) -> Iterator[complex]:
         yield move
 
 
+def to_steps(move: complex) -> Iterator[complex]:
+    """Yield a series of steps in a move.
+
+    For example, steps(3j) yields 1j, 1j, and 1j.
+    """
+    magnitude = int(abs(move))
+    step = move / magnitude
+    for _ in range(magnitude):
+        yield step
+
+
 def walk(wire: str) -> Iterator[complex]:
     """Walk a wire and yield each point visited, excluding the starting point.
 
@@ -29,17 +40,6 @@ def walk(wire: str) -> Iterator[complex]:
         for step in to_steps(move):
             head += step
             yield head
-
-
-def to_steps(move: complex) -> Iterator[complex]:
-    """Yield a series of steps in a move.
-
-    For example, steps(3j) yields 1j, 1j, and 1j.
-    """
-    magnitude = int(abs(move))
-    step = move / magnitude
-    for _ in range(magnitude):
-        yield step
 
 
 def intersections(w1: str, w2: str) -> Set[complex]:
@@ -53,13 +53,11 @@ def manhattan_dist(a: complex, b: complex) -> int:
 
 def part_1(w1: str, w2: str) -> int:
     """Return the Manhattan distance to the most central intersection."""
-    points = intersections(w1, w2)
     heuristic = lambda p: manhattan_dist(0 + 0j, p)
-    return min(map(heuristic, points))
+    return min(map(heuristic, intersections(w1, w2)))
 
 
 def part_2(w1: str, w2: str) -> int:
-    # Brute-force, with much more big-O space than needed :(
     p1 = list(walk(w1))
     p2 = list(walk(w2))
     # Add 2, since .index() starts at 0.
